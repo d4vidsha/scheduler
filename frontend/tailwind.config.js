1a3a426ae16456eaedfe7cbe8e7b08a5be9effa1
David Sha <david.sha2002@gmail.com>
2024-06-30 08:10:05 +1000

    feat: add inter var font family as per https://tailwindui.com/documentation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
