b4acc6d4efa4fd30a6ab4ec83b1041e18ccbc4f5
David Sha <david.sha2002@gmail.com>
2024-07-01 18:59:46 +1000

    feat: replace chakra components

--- frontend/src/routes/_layout.tsx
+++ frontend/src/routes/_layout.tsx
@@ -1,9 +1,9 @@
-import { Flex, Spinner } from "@chakra-ui/react"
 import { Outlet, createFileRoute, redirect } from "@tanstack/react-router"
 
 import Sidebar from "../components/Common/Sidebar"
 import UserMenu from "../components/Common/UserMenu"
 import useAuth, { isLoggedIn } from "../hooks/useAuth"
+import { Spinner } from "@/components/Spinner"
 
 export const Route = createFileRoute("/_layout")({
   component: Layout,
@@ -20,16 +20,16 @@ function Layout() {
   const { isLoading } = useAuth()
 
   return (
-    <Flex maxW="large" h="auto" position="relative">
+    <div className="flex max-w-large h-auto relative">
       <Sidebar />
       {isLoading ? (
-        <Flex justify="center" align="center" height="100vh" width="full">
-          <Spinner size="xl" color="ui.main" />
-        </Flex>
+        <div className="flex justify-center items-center h-screen w-full">
+          <Spinner size="large" />
+        </div>
       ) : (
         <Outlet />
       )}
       <UserMenu />
-    </Flex>
+    </div>
   )
 }
