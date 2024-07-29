35f3b119d788ee726fe353e3c38062f2413407fb
David Sha <david.sha2002@gmail.com>
2024-07-29 16:40:19 +1000

    feat: animate hover on checkbox
    This commit uses a grid container to overlap two icons. Additionally,
    the hover and tap animations are done with CSS and Framer Motion
    respectively. I learnt a lot about grid layouts because of this commit.

--- frontend/src/routes/_layout/tasks.tsx
+++ frontend/src/routes/_layout/tasks.tsx
@@ -1,16 +1,20 @@
 import { createFileRoute } from "@tanstack/react-router"
-import { Circle } from "lucide-react"
+import { motion } from "framer-motion"
+import { Check, Circle } from "lucide-react"
 
 export const Route = createFileRoute("/_layout/tasks")({
   component: Tasks,
 })
 
 const tasks = [
-  { id: 1, title: "Study for 3 hours" },
-  { id: 2, title: "Create task list" },
-  { id: 3, title: "Clean bedroom" },
-  { id: 4, title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." }
+  { id: 1, title: "Study for 3 hours", completed: true },
+  { id: 2, title: "Create task list", completed: false },
+  { id: 3, title: "Clean bedroom", completed: false },
+  { id: 4, title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", completed: false }
 ]
+
+function onCheckmarkTap(event, info) {
+}
 
 function Tasks() {
   return (
@@ -24,10 +28,18 @@ function Tasks() {
           </header>
           <main className="flex flex-col">
             {tasks.map((task) => (
-              <div key={task.id.toString()} className="flex shrink-1 border-b py-3">
-                <button className="self-start">
-                  <Circle className="flex-none h-5 w-5 mr-2 transition-colors hover:bg-accent rounded-full" />
-                </button>
+              <div key={task.id.toString()} className="flex gap-2 shrink-1 border-b py-3">
+                <motion.button
+                  whileTap={{ scale: 1.2 }}
+                  onTap={onCheckmarkTap}
+                  className="flex-none self-start">
+                  <div className="grid grid-cols-1 grid-rows-1">
+                    <Circle className="h-5 w-5 row-start-1 row-end-1 col-start-1 col-end-1" />
+                    <div className="group flex justify-center items-center row-start-1 row-end-1 col-start-1 col-end-1" >
+                      <Check strokeWidth={4.5} className="h-3 w-3 opacity-0 group-hover:opacity-100 transition-opacity ease-in-out duration-300" />
+                    </div>
+                  </div>
+                </motion.button>
                 <p className="text-sm line-clamp-4 text-ellipsis">{task.title}</p>
               </div>
             ))}
