a47f4b8b41b96e054f9e30695b9afcb43e25006d
David Sha <70577814+d4vidsha@users.noreply.github.com>
2025-05-25 13:50:47 +1000

    Implement adding tasks; show tasks on Calendar page. (#57)
    * feat: add tasks component to calendar page
    
    Also introduce resizeable from shadcn.
    
    * chore: set Python version
    
    * feat: add tasks
    
    * chore: use lower Python version
    
    * chore: autogenerate frontend client
    
    * chore: format
    
    * fix: remove TaskCreate
    
    * chore: autogenerate frontend client
    
    * fix: type checks
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- frontend/src/routes/_layout/index.tsx
+++ frontend/src/routes/_layout/index.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router"
-
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable"
 import Calendar from "@/components/calendar"
+import Tasks from "./tasks"
 
 export const Route = createFileRoute("/_layout/")({
   component: Dashboard,
@@ -8,8 +9,16 @@ export const Route = createFileRoute("/_layout/")({
 
 function Dashboard() {
   return (
-    <div className="overflow-hidden h-[calc(100vh-65px)]">
-      <Calendar />
+    <div className="h-[calc(100vh-65px)] w-full overflow-hidden">
+      <ResizablePanelGroup direction="horizontal" className="h-full w-full">
+        <ResizablePanel defaultSize={35} className="min-w-[300px] max-w-[600px]">
+          <Tasks />
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={65} className="min-w-[400px]">
+          <Calendar />
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   )
 }
